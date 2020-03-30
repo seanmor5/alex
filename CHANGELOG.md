df2034dc6606a8b6432af7876e1d8ca80933be97
Sean Moriarity <smoriarity.5@gmail.com>
2020-03-29 21:34:12 -0400

    change compile.ale to compiler task

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# v0.1.0
+
+# v0.2.0
+* Changed location of libale_c target to `priv`.
+* Changes to `mix.exs` due to compilation issues.
+
+# v0.3.0
+* Fixed double compilation bug.
+* Stopped shipping libale_c with package.
+* Changed location of `compile.ale.ex`.
+* Added Safety Checks to `get_` and `set_` NIFs so they no longer SegFault when given bad arguments.
--- mix.exs
+++ mix.exs
@@ -1,3 +1,28 @@
+defmodule Mix.Tasks.Compile.Ale do
+  use Mix.Task.Compiler
+
+  def run(_args) do
+    path_to_ale = Path.join(["src", "ale", "build"])
+
+    if File.exists?(path_to_ale) do
+      IO.write("ALE Already Compiled.\n")
+    else
+      IO.write("Compiling ALE. This will take some time.\n")
+      with {_result, 0} <- System.cmd("mkdir", [path_to_ale], stderr_to_stdout: true),
+           :ok <- File.cd(path_to_ale),
+           {_result, 0} <- System.cmd("cmake", ["-DUSE_SDL=ON", ".."], stderr_to_stdout: true),
+           {result, 0} <- System.cmd("make", ["-j", "4"], stderr_to_stdout: true) do
+        IO.binwrite result
+        if Version.match?(System.version(), "~> 1.9"), do: {:ok, []}, else: :ok
+      else
+        {result, err_code} ->
+          IO.binwrite result
+          {:error, result, err_code}
+      end
+    end
+  end
+end
+
 defmodule Alex.MixProject do
   use Mix.Project
 
