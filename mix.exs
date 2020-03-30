32c896c90c0eb3dcc46b6e1832b6fdb532ea91d8
Sean Moriarity <smoriarity.5@gmail.com>
2020-03-29 21:44:34 -0400

    fix the mix compiler

--- mix.exs
+++ mix.exs
@@ -3,17 +3,16 @@ defmodule Mix.Tasks.Compile.Ale do
 
   def run(_args) do
     path_to_ale = Path.join(["src", "ale", "build"])
-
+    IO.write("Compiling ALE. This will take some time.\n")
     if File.exists?(path_to_ale) do
       IO.write("ALE Already Compiled.\n")
     else
-      IO.write("Compiling ALE. This will take some time.\n")
       with {_result, 0} <- System.cmd("mkdir", [path_to_ale], stderr_to_stdout: true),
            :ok <- File.cd(path_to_ale),
            {_result, 0} <- System.cmd("cmake", ["-DUSE_SDL=ON", ".."], stderr_to_stdout: true),
            {result, 0} <- System.cmd("make", ["-j", "4"], stderr_to_stdout: true) do
         IO.binwrite result
-        if Version.match?(System.version(), "~> 1.9"), do: {:ok, []}, else: :ok
+        :ok
       else
         {result, err_code} ->
           IO.binwrite result
